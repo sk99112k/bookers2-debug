844c41b0018e57208fdde3bbfb81aa520c34c7f7
EC2 Default User <ec2-user@ip-172-31-28-117.ap-northeast-1.compute.internal>
2023-04-18 00:05:15 +0000

    0418 09:00

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,10 +2,12 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
@@ -15,7 +17,7 @@ class BooksController < ApplicationController
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
-      render 'index'
+      render :index
     end
   end
 
@@ -28,7 +30,7 @@ class BooksController < ApplicationController
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
-      render "edit"
+      render :edit
     end
   end
 
@@ -41,6 +43,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title,:body)
   end
 end
